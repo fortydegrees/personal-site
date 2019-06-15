65f812961e078956d0c2e9c50433c4b64ff5952e
David Pratt <david@david-pratt.com>
2019-06-15 12:00:39 +0100

    styling

--- src/components/WeblogFeed/WeblogFeed.js
+++ src/components/WeblogFeed/WeblogFeed.js
@@ -14,12 +14,14 @@ const WeblogFeed = ({ edges }: Props) => (
     {edges.map((edge) => (
       <div>
          <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
-            {moment(edge.node.frontmatter.date).format('MMMM YYYY')}
+            {moment(edge.node.frontmatter.date).format('MMM DD \'YY')}
           </time>
           <span className={styles['feed__item-meta-divider']} />
+        {edge.node.frontmatter.title && 
         <h2 className={styles['feed__item-title']}>
           {edge.node.frontmatter.title}
         </h2>
+        }
         <div className={styles['post__content']}>
         <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: edge.node.html }} />
       </div>
